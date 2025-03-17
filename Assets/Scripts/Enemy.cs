777d0fe1eba8e9b2f27a1348b24c91d55f738974
Dylan <dylanjohnson4070@gmail.com>
2025-03-17 11:37:32 -0500

    Added Basic Enemy Spawning Functionality, Bullet Despawn, Life, Score

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = (float)2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * -speed * Time.deltaTime);
+        if (transform.position.x < -7.5)
+        {
+            Destroy(gameObject);
+            Game.Health--;
+        }
+
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            Game.Score++;
+        }
+    }
+}
