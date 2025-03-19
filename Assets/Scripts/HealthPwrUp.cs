78f9bd66d28f2cf56267fc4a212cf9cd57027f1c
Milo <milohatten@gmail.com>
2025-03-19 10:23:22 -0500

    more health stuff

--- Assets/Scripts/HealthPwrUp.cs
+++ Assets/Scripts/HealthPwrUp.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthPwrUp : MonoBehaviour
-{
+public class HealthPwrUp : MonoBehaviour{
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         GetComponent<Rigidbody2D>().AddForce(Vector2.left * Random.Range(100f, 500f));
         Vector3 initPos = new Vector3(Random.Range(-8, 6), 6, 0);
         transform.position = initPos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision){
-        if (collision.GetComponent<Player>() != null){
-            Destroy(GameObject);
-            updateHealth(1);
-            print("Health up!");
+    // Update is called once per frame
+    void Update(){
+        transform.Translate(Vector3.down * Time.deltaTime);
+        if (transform.position.y < -6){
+            Destroy(gameObject);
         }
     }
+
+    // void OnTriggerEnter2D(Collider2D collision){
+    //     if (collision.CompareTag("Player")){
+    //         Destroy(gameObject);
+    //     }
+    // }
+
 }
