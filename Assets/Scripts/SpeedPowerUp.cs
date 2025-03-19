56d8b558cd698080491a2cf7c593be6b956554ea
Milo <milohatten@gmail.com>
2025-03-19 12:02:46 -0500

    Finished updating speed and health powerups

--- Assets/Scripts/SpeedPowerUp.cs
+++ Assets/Scripts/SpeedPowerUp.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpeedPowerUp : MonoBehaviour{
     // Start is called before the first frame update
     void Start(){
-        GetComponent<Rigidbody2D>().AddForce(Vector2.left * Random.Range(100f, 500f));
+        //GetComponent<Rigidbody2D>().AddForce(Vector2.left * Random.Range(100f, 500f));
         Vector3 initPos = new Vector3(Random.Range(-8, 6), 6, 0);
         transform.position = initPos;
     }
@@ -15,6 +15,12 @@ public class SpeedPowerUp : MonoBehaviour{
     void Update(){
         transform.Translate(Vector3.down * Time.deltaTime);
         if (transform.position.y < -6){
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.CompareTag("Player")){
             Destroy(gameObject);
         }
     }
