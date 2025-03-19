5af23a82d4623a09b817ee60d1fcad3a6cb22b36
Milo <milohatten@gmail.com>
2025-03-19 09:41:05 -0500

    More powerup

--- Assets/Scripts/HealthPwrUp.cs
+++ Assets/Scripts/HealthPwrUp.cs
@@ -17,4 +17,12 @@ public class HealthPwrUp : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.GetComponent<Player>() != null){
+            Destroy(GameObject);
+            updateHealth(1);
+            print("Health up!");
+        }
+    }
 }
