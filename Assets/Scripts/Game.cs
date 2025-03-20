60637eccb5539f2a329b9a2f8bf2163972176c66
Dylan <dylanjohnson4070@gmail.com>
2025-03-20 18:28:37 -0500

    Fixed power up spawning

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -19,7 +19,7 @@ using Random =  UnityEngine.Random;
     //public int last = 0;  
     public int curr = 0; 
     public int powerUp; //determine which powerup is spawned
-
+    public Boolean gameStarted = false;
     #region properties
     public static SpaceShooterControls Input { get; private set; }
     public static Game Instance { get; private set; }
@@ -46,27 +46,36 @@ using Random =  UnityEngine.Random;
 
     // Update is called once per frame
     private void Update() {
-        scoreText.text = _score.ToString("00000");
-        
-        if (_powerUpTimer == 0 || (_powerUpTimer == float.MaxValue))
+        if (gameStarted)
         {
-            _powerUpTimer = Random.Range(5f, 12f);
-        }
-        curr =  (int)Time.time;
+            scoreText.text = _score.ToString("00000");
 
-        //controls spawning of power ups
-        if ((curr - _powerUpTimer) > 0){
-            powerUp = Random.Range(0, 2);
-            if (powerUp == 0){
-                Instantiate(healthPowerUpPrefab);
-                
+            if (_powerUpTimer == 0 || (_powerUpTimer == float.MaxValue))
+            {
+                _powerUpTimer = Random.Range(5f, 12f);
             }
-            else if (powerUp == 1){
-                Instantiate(speedPowerUpPrefab);
+
+            curr = (int)Time.time;
+
+            //controls spawning of power ups
+
+            if ((curr - _powerUpTimer) > 0)
+            {
+                powerUp = Random.Range(0, 2);
+                if (powerUp == 0)
+                {
+                    Instantiate(healthPowerUpPrefab);
+
+                }
+                else if (powerUp == 1)
+                {
+                    Instantiate(speedPowerUpPrefab);
+                }
+
+                _powerUpTimer += Random.Range(5f, 12f);
             }
-            _powerUpTimer += Random.Range(5f, 12f);
         }
-        
+
     }
 
     public void StartGame(GameObject mainMenuScreen)
@@ -74,6 +83,7 @@ using Random =  UnityEngine.Random;
         Input.Enable();
         mainMenuScreen.SetActive(false);
         _score = 0;
+        gameStarted = true;
         
     }
 
